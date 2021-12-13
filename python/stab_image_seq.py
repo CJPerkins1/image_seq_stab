0fe09e4c99fdbaf2402d10663ffc6643c523cc2d
cedarwarman <cedardalewarman@gmail.com>
2021-12-13 12:01:53 -0700

    Fix frame sync problem, fix path name

--- python/stab_image_seq.py
+++ python/stab_image_seq.py
@@ -6,6 +6,7 @@
 ### Imports
 from vidstab.VidStab import VidStab
 import cv2
+import numpy as np
 import glob
 import shutil
 import os
@@ -53,8 +54,14 @@ def open_images_and_stabilizer():
     # Starting up the stabilizer 
     stabilizer = VidStab()
 
-    # Opening the image sequence
-    image_seq = cv2.VideoCapture('2021-11-08_plate1_34C_A1_t%03d.tif')
+    # Opening the image sequence (just looking at pwd for now)
+    # Start by getting the string of the first tif, minus the number and the 
+    # extension.
+    image_prefix = sorted(glob.glob('*.tif'))[0][:-8]
+
+    # Add the 't%03d.tif' ending for cv2.VideoCapture
+    vid_capture_string = image_prefix + 't%03d.tif'
+    image_seq = cv2.VideoCapture(vid_capture_string)
     
     # Making sure the image sequence opened ok. 
     if (image_seq.isOpened() == False):
@@ -62,6 +69,21 @@ def open_images_and_stabilizer():
 
     return stabilizer, image_seq
 
+
+### Remove dark or absent frames
+# There's a problem with the microscope where sometimes it will capture very
+# dark or missing images. This function checks for them, because they break the
+# image stabilization. It returns TRUE if it's a good image and FALSE if it's
+# dark or missing. It's called inside the stabilization function.
+def check_image_quality(input_image):
+    average_pixel_value = np.average(input_image)
+    print("Average pixel value: ", average_pixel_value)
+
+    # For the final version make a more sophisticated version of this
+    if average_pixel_value > 50:
+        return True
+    else:
+        return False
 
 ### Stabilize and write images
 # This function stabilizes and writes out stabilized images.
@@ -71,33 +93,72 @@ def stabilize_and_write_images(stabilizer, image_seq, image_names, image_num):
     
     # Setting a counter to keep track of the actual images
     image_counter = 0
+
+    # Setting up an empty list to store the images that fail the quality check
+    failed_images = []
     
+    # Doing the stabilization
     while True:
+        # It gets a frame whether it stabilizes it or not. Can I got back in
+        # time if it doesn't stabilize it? What's happening here? Where does
+        # the extra frame go?
         grabbed_frame, frame = image_seq.read()
+
+        print("Frame counter is ", counter)
+        print("Image counter for writing is ", image_counter)
+        print("Frame type is ", type(frame))
+
+        # This is a little sloppy, but if it's out of rames in the .read() 
+        # then it will break here (happens at the end of the image seq).
+        if frame is None:
+             break
     
-        #if frame is not None:
-        #    # Perform any pre-processing of frame before stabilization here
-        #    pass
+        if check_image_quality(frame):
+            print("running stabilizer")
+            # stabilized_frame will be an all black frame until iteration 30
+            # Doing the actual stabilization
+            stabilized_frame = stabilizer.stabilize_frame(input_frame = frame,
+                                                          smoothing_window = 30)
     
-        # Pass frame to stabilizer even if frame is None
-        # stabilized_frame will be an all black frame until iteration 30
-    
-        # Doing the actual stabilization
-        stabilized_frame = stabilizer.stabilize_frame(input_frame = frame,
-                                                      smoothing_window = 30)
-    
+        if not check_image_quality(frame):
+            failed_image = image_names[counter]
+            print(failed_image, 
+                  "failed quality check. Added to failed image list.") 
+            failed_images.append(counter)
+            print(failed_images)
+            continue
+
+        # Check to see if frame is on skipped list here
         if counter >= 30 and counter < image_num + 30:
+            if image_counter not in failed_images:
+                current_image = image_names[image_counter]
+                current_image_no_extension = current_image[:len(current_image)-4]
+                stabilized_name = current_image_no_extension + "_stab.tif"
     
-            #print("image is ", image_names[image_counter])
-            current_image = image_names[image_counter]
-            current_image_no_extension = current_image[:len(current_image)-4]
-            stabilized_name = current_image_no_extension + "_stab.tif"
-    
-            #cv2.waitKey(0)
-    
-            cv2.imwrite(stabilized_name, stabilized_frame)
-    
-            image_counter += 1
+                print("Stabilized image is ", stabilized_name, '\n\n')
+                cv2.imwrite(stabilized_name, stabilized_frame)
+
+                image_counter += 1
+
+            else:
+                # This is all just for the print, not really necessary
+                current_image = image_names[image_counter]
+                current_image_no_extension = current_image[:len(current_image)-4]
+                stabilized_name = current_image_no_extension + "_stab.tif"
+                print("Skipping unstabilized image ", stabilized_name)
+
+                # Skips the image that wasn't stabilized for naming
+                image_counter += 1
+                current_image = image_names[image_counter]
+                current_image_no_extension = current_image[:len(current_image)-4]
+                stabilized_name = current_image_no_extension + "_stab.tif"
+
+                print("Stabilized image is ", stabilized_name, '\n\n')
+                cv2.imwrite(stabilized_name, stabilized_frame)
+
+                image_counter += 1
+                counter += 1
+                continue
     
         if stabilized_frame is None:
             # There are no more frames available to stabilize
@@ -110,14 +171,6 @@ def stabilize_and_write_images(stabilizer, image_seq, image_names, image_num):
 def clean_fake_frames(fake_names):
     for file in fake_names:
         os.remove(file)
-
-### Remove dark or absent frames
-# There's a problem with the microscope where sometimes it will capture very
-# dark or missing images. This function checks for them, because they break the
-# image stabilization. It returns TRUE if it's a good image and FALSE if it's
-# dark or missing. It's called inside the stabilization function.
-def check_image_quality(input_image):
-    pass
 
 
 ### Here's the main
